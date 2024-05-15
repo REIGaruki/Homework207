7bf29481d7e25ff0ef9ed8f9bf33c5993df90434
REIGaruki <gyakujo@mail.ru>
2024-05-15 22:37:15 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(4, 8, 15, 16, 23, 42, 37, 23, 57, 4, 9, 3, 7));
         for (Integer num : nums) {
             if (num % 2 == 1) {
                 System.out.println(num);
             }
         }
         System.out.println("Task 2");
-        for (int i = Collections.min(nums); i <= Collections.max(nums); i+= 1) {
+        for (int i = Collections.min(nums); i <= Collections.max(nums);) {
             if (nums.contains(i) && i % 2 == 0) {
                 System.out.println(i);
+                i+= 2;
+            } else {
+                i++;
             }
         }
     }
